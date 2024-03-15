6f7bf23ff11f3819e19d8e19c5baa0989a58a4e7
cwest52 West <cmw5219846@lpssonline.com>
2024-03-15 03:39:47 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -20,8 +20,6 @@ function runProgram(){
     d: 68,
     s: 83,
   }
-  var positionX = 0;
-  var speedX = 0; 
   // Game Item Objects
 
   var walker = {
@@ -29,13 +27,15 @@ function runProgram(){
     y: 10, // the y-coordinate location for the box
     xSpeed: 0, // the speed for the box along the x-axis
     ySpeed: 0, // the speed for the box along the y-axis
+    it: -1
   }
 
   var playTwo = {
-    x: 389,
-    y: 389,
+    x: 380,
+    y: 380,
     xSpeed: 0,
     ySpeed: 0,
+    it: 1
   }
 
   // one-time setup
@@ -51,9 +51,13 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
+var tick = 0
   function newFrame() {
+    tick += 1
     repositionGameItem()
     wallCollision()
+    playerCollision()
+    isIt()
     redrawGameItem()
   }
   
@@ -135,8 +139,8 @@ function runProgram(){
   function redrawGameItem(){
     $("#walker").css("left", walker.x); // draw the walker in the new location, walker.x pixels away from the "left"
     $("#walker").css("top", walker.y); // draw the walker in the new location, walker.y pixels away from the "top"
-    $("playTwo").css("left", playTwo.x)
-    $("playTwo").css("top", playTwo.y)
+    $("#playTwo").css("left", playTwo.x)
+    $("#playTwo").css("top", playTwo.y)
   }
 boardWandH = $("#board").width()
   function wallCollision(){
@@ -163,6 +167,27 @@ boardWandH = $("#board").width()
     }
     if (playTwo.y === 390) { // the height of the board - the height of the walker (It didn't work when I tried to soft-code it, and I tried about 5 different combinations)
       playTwo.y -= playTwo.ySpeed
+    }
+  }
+  function playerCollision(){
+    for (var i = -50; i < 50; i++){
+      if (walker.x === playTwo.x - i){
+        for (var j = -50; j < 50; j++){
+          if (walker.y === playTwo.y - j) {
+            walker.it *= -1
+            playTwo.it *= -1
+          }
+        }
+      }
+    }
+  }
+  function isIt(){
+    if (walker.it === 1) 
+      $("#walker").css("background", "red")
+      $("#playTwo").css("background", "cyan")
+    } else if (playTwo.it === 1) {
+      $("#walker").css("background", "cyan")
+      $("#playTwo").css("background", "red")
     }
   }
 }
