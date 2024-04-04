2190676128556a4afd04292d6726a9ba16b7d95e
cwest52 West <cmw5219846@lpssonline.com>
2024-04-04 15:21:07 +0000

    saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
+  applyFilterNoBackground(reddify)
 
   // do not change the below line of code
   render($("#display"), image);
@@ -33,17 +32,52 @@ function applyAndRender() {
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[j].length; j++) {
+      rgbString = image[i][j]
+      rgbNumbers = rgbStringToArray(rgbString)
+      filterFunction(rgbNumbers)
+      rgbString = rgbArrayToString(rgbNumbers)
+      image[i][j] = rgbString
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
+function applyFilterNoBackground(filterFunction) {
+  backGroundColor = image[0][0]
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[j].length; j++) {
+      if (image[i][j] !== backGroundColor) {
+        rgbString = image[i][j]
+        rgbNumbers = rgbStringToArray(rgbString)
+        filterFunction(rgbNumbers)
+        rgbString = rgbArrayToString(rgbNumbers)
+        image[i][j] = rgbString
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(a) {
+  return a < 0 ? 0 : a > 255 ? 255 : a
+}
 
 // TODO 3: Create reddify function
 
+function reddify(arr) {
+  arr[RED] = 200
+}
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(a){
+  a[BLUE] = keepInBounds(a[BLUE] - 50);
+}
+function increaseGreenByBlue(a) {
+  a[GREEN] = keepInBounds(a[BLUE] + a[GREEN]);
+}
 
 // CHALLENGE code goes below here
